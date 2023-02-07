7c6cb621695668d083ec12fd1a33b534eced4df7
Voltonik <markasaadramzy@gmail.com>
2023-02-07 23:09:54 +0200

    Added validators for Question model

--- exams/admin.py
+++ exams/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 from django import forms
-
-from utils.widgets import *
+from django.core.exceptions import ValidationError
 
 from .models import *
-
 
 class QuestionAdminForm(forms.ModelForm):
     class Meta:
@@ -17,26 +15,43 @@ class QuestionAdminForm(forms.ModelForm):
                 }
             ),
         }
-
+        
     class Media:
         js = ("js/hideshow.js",)
-
+        
+    def clean(self):
+        cleaned_data = super(QuestionAdminForm, self).clean()
+        
+        type = cleaned_data.get('type')
+        
+        if type == QuestionTypes[0][0]:
+            self.validate_required_field(cleaned_data, ['choice_A', 'choice_B', 'choice_C', 'choice_D'])
+            
+            true_choices = [is_true for is_true in [cleaned_data.get('is_true_A'), cleaned_data.get('is_true_B'), cleaned_data.get('is_true_C'), cleaned_data.get('is_true_D')] if is_true == True]
+            
+            if len(true_choices) is not 1:
+                raise ValidationError('Only one choice should be correct.')
+            
+        return self.cleaned_data
+        
+    def validate_required_field(self, cleaned_data, field_names=[], message="This field is required."):
+        for field_name in field_names:
+            if(field_name in cleaned_data and (cleaned_data[field_name] is None or cleaned_data[field_name] is '')):
+                self._errors[field_name] = self.error_class([message])
+                del cleaned_data[field_name]
 
 class QuestionAdmin(admin.ModelAdmin):
     form = QuestionAdminForm
 
-
 class QuestionInline(admin.StackedInline):
     model = Question
     form = QuestionAdminForm
-    extra = 2
-
+    extra = 0
 
 class ExamAdmin(admin.ModelAdmin):
     inlines = [
         QuestionInline
     ]
-
 
 admin.site.register(Subject)
 admin.site.register(Exam, ExamAdmin)
--- exams/models.py
+++ exams/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django import forms
 
 from accounts.models import SchoolGrades
 
+QuestionTypes = [(0, "Multiple Choice"), (1, "True or False")]
 
 class Subject(models.Model):
     name = models.CharField(max_length=255, default="Subject Name")
@@ -9,38 +11,33 @@ class Subject(models.Model):
     def __str__(self):
         return self.name
 
-
 class Exam(models.Model):
-    subject = models.OneToOneField(Subject, on_delete=models.CASCADE)
+    subject = models.OneToOneField(Subject, on_delete=models.CASCADE, null=False)
     scheduled_for = models.DateTimeField()
-    for_grade = models.CharField(
-        choices=SchoolGrades.choices, default=SchoolGrades.G01, max_length=3)
+    for_grade = models.CharField(choices=SchoolGrades.choices, default=SchoolGrades.G01, max_length=3)
 
     def __str__(self):
         return f"{self.subject} ({self.for_grade})  {self.scheduled_for}"
 
-
 class Question(models.Model):
-    CHOICES = [(0, "Multiple Choice"), (1, "True or False")]
-
     exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
 
     prompt = models.TextField(max_length=52000)
     mark = models.DecimalField(default=0, max_digits=4, decimal_places=3)
 
-    type = models.IntegerField(choices=CHOICES)
+    type = models.IntegerField(choices=QuestionTypes, default=0, null=False)
 
     # MCQ
-    choice_A = models.TextField(max_length=1024)
+    choice_A = models.TextField(max_length=1024, blank=True)
     is_true_A = models.BooleanField(default=False)
 
-    choice_B = models.TextField(max_length=1024)
+    choice_B = models.TextField(max_length=1024, blank=True)
     is_true_B = models.BooleanField(default=False)
 
-    choice_C = models.TextField(max_length=1024)
+    choice_C = models.TextField(max_length=1024, blank=True)
     is_true_C = models.BooleanField(default=False)
 
-    choice_D = models.TextField(max_length=1024)
+    choice_D = models.TextField(max_length=1024, blank=True)
     is_true_D = models.BooleanField(default=False)
 
     # True or False
