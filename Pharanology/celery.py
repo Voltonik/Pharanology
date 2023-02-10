7e1048d3c97b89f1599054c399199d7450a07ba4
Voltonik <markasaadramzy@gmail.com>
2023-02-10 16:33:20 +0200

    Post-merge

--- Pharanology/celery.py
+++ Pharanology/celery.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+import redis #Only importing to be included in requirements.txt auto-generation.
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Pharanology.settings')
--- Pharanology/settings.py
+++ Pharanology/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sessions',
-    'Pharanology',
+
     'accounts',
     'exams'
 ]
