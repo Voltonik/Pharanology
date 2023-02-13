0174b0044f087cfc72d7f812f4dcb7b478266af2
Maged Ibrahim <magedibra@yahoo.com>
2023-02-13 09:17:23 +0200

    Add Reset Password Sent

--- frontend/src/pages/Authentication/ResetPassword/ResetPasswordSent.jsx
+++ frontend/src/pages/Authentication/ResetPassword/ResetPasswordSent.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
+import { sectionHeight100vh } from "@/utils.js";
+import "./reset-password-sent.scss";
+// Components
+import AuthenticationModal from "@components/AuthenticationModal/AuthenticationModal";
 
 function ResetPasswordSent() {
-  return <div>ResetPasswordSent</div>;
+  const container = useRef(null);
+  useEffect(() => {
+    sectionHeight100vh(container.current);
+    document.addEventListener("resize", () => {
+      sectionHeight100vh(container.current);
+    });
+  }, []);
+  return (
+    <div id="reset-password-sent" ref={container}>
+      <AuthenticationModal hasShadow={true}>
+        <h3 className="text-success">Password reset sent</h3>
+
+        <p>
+          We've emailed you instructions for settings your password, if an
+          account exists with the email you entered. You should receive them
+          shortly.
+        </p>
+
+        <p>
+          If you don't receive an email, please make sure you've entered the
+          address you registered with, and check your spam folder.
+        </p>
+      </AuthenticationModal>
+    </div>
+  );
 }
 
 export default ResetPasswordSent;
