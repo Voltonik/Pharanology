073562064a580ebd3897d318da87828191f8bc4b
Maged Ibrahim <magedibra@yahoo.com>
2023-02-14 09:40:25 +0200

    Add Why us section

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "@sections/Hero/Hero.jsx";
+import WhyUs from "@sections/WhyUs/WhyUs.jsx";
 
 function Home() {
   return (
     <>
       <Hero />
+      <WhyUs />
     </>
   );
 }
