7059d5bfb7fe499381ea276c0ab44700119adcf5
Maged Ibrahim <magedibra@yahoo.com>
2023-02-17 06:38:19 +0200

    Constumize Error Page

--- frontend/src/pages/Error/Error.jsx
+++ frontend/src/pages/Error/Error.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import Height100vh from "@components/Height100vh/Height100vh.jsx";
 import "./error.scss";
 
-function Error() {
+function Error({ message, code, children }) {
   return (
     <Height100vh className="error">
       <Height100vh className="container">
-        <h2 className="text-danger">Error 404</h2>
-        <h4 className="text-danger">Page has not been found</h4>
+        <h2 className="text-danger">Error {code || 404}</h2>
+        <h4 className="text-danger">{message || "Page has not been found"}</h4>
+        {children}
       </Height100vh>
     </Height100vh>
   );
