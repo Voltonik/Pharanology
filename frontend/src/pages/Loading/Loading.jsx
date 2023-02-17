ac6d3e53b6589a322043e7c2d6f631a88cf7e2c7
Maged Ibrahim <magedibra@yahoo.com>
2023-02-17 07:26:29 +0200

    Change background

--- frontend/src/pages/Loading/Loading.jsx
+++ frontend/src/pages/Loading/Loading.jsx
@@ -7,10 +7,12 @@ import Spinner from "react-bootstrap/Spinner";
 
 function Loading() {
   return (
-    <Height100vh className="loading  container d-flex justify-content-center align-items-center flex-column">
-      <h3>Loading...</h3>
-      <p className="opacity-75">Loading content. Please wait</p>
-      <Spinner animation="border" variant="primary" role="status" />
+    <Height100vh className="loading">
+      <Height100vh className="container d-flex justify-content-center align-items-center flex-column">
+        <h3>Loading...</h3>
+        <p className="opacity-75">Loading content. Please wait</p>
+        <Spinner animation="border" variant="primary" role="status" />
+      </Height100vh>
     </Height100vh>
   );
 }
