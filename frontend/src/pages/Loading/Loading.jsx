db765cd476d6011b246c746d4e1eec608f58611d
Maged Ibrahim <magedibra@yahoo.com>
2023-02-17 07:05:29 +0200

    Add Loading Page

---
+++ frontend/src/pages/Loading/Loading.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./loading.scss";
+// Components
+import Height100vh from "@components/Height100vh/Height100vh";
+// react-bootstrap
+import Spinner from "react-bootstrap/Spinner";
+
+function Loading() {
+  return (
+    <Height100vh className="loading  container d-flex justify-content-center align-items-center flex-column">
+      <h3>Loading...</h3>
+      <p className="opacity-75">Loading content. Please wait</p>
+      <Spinner animation="border" variant="primary" role="status" />
+    </Height100vh>
+  );
+}
+
+export default Loading;
