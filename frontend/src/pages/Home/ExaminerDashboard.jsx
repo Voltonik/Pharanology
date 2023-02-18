41669aaa222db0ad1a0d0c5697647ac4b0a71ef2
Maged Ibrahim <magedibra@yahoo.com>
2023-02-18 14:05:11 +0200

    Create ExaminerDashboard

--- frontend/src/pages/Home/ExaminerDashboard.jsx
+++ frontend/src/pages/Home/ExaminerDashboard.jsx
@@ -1,11 +1,31 @@
 import React from "react";
 import "./examiner-dashboard.scss";
 import Height100vh from "@components/Height100vh/Height100vh.jsx";
+// react-router-dom
+import { Link } from "react-router-dom";
+// context
+import { useAuthentication } from "@context/AuthenticationContext";
 
 function ExaminerDashboard() {
+  const { userData } = useAuthentication();
+  const fullName = `${userData.first_name} ${userData.last_name}`;
   return (
-    <Height100vh id="examiner-dashboard">
-      <div className="container"></div>
+    <Height100vh id="student-dashboard">
+      <div className="container">
+        <div className="row welcoming-user">
+          <span>
+            <h2>Hello,</h2>
+            <span className="fs-4 fw-normal text-black opacity-50">
+              {fullName}.
+            </span>
+          </span>
+        </div>
+        <div className="row new-exam-row">
+          <Link to="/new/exam" className="btn btn-primary">
+            Create a new exam
+          </Link>
+        </div>
+      </div>
     </Height100vh>
   );
 }
